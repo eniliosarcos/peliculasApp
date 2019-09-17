a35569e85a38749d894e18d6fd33a2396b093cd8
eniliosarcos <eniliosarcos1993@gmail.com>
2019-09-17 13:58:43 -0400

    class 101 movies providers

---
+++ lib/src/providers/peliculas.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:peliculas/src/models/peliculas.dart';
+import 'package:http/http.dart' as http;
+
+class PeliculasProvider
+{
+  String _apiKey = '428282617d08952c4a3ca4d28f4ce30b';
+  String _url = 'api.themoviedb.org';
+  String _lenguage = 'es-ES';
+
+  Future<List<Pelicula>> getInCines() async
+  {
+    final url = Uri.https(_url, '3/movie/now_playing',{
+      'apiKey' : _apiKey,
+      'language': _lenguage
+    });
+
+    final respuesta = await http.get(url);
+    final decodedData = json.decode(respuesta.body);
+
+    final pelicula = new Peliculas.jsonFromList(decodedData['results']);
+
+    return pelicula.items;
+
+  }
+}
