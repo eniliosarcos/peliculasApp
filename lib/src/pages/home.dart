c6b2d95907ce1c5ae2adb24e1a19f27ccbabf011
eniliosarcos <eniliosarcos1993@gmail.com>
2019-09-17 12:47:39 -0400

    class 98 custom widget - cardSwiper

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:peliculas/src/widgets/cardSwiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -29,21 +29,8 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperTarjetas() {
-    return Container(
-      padding: EdgeInsets.only(top: 10.0),
-      width: double.infinity,
-      height: 300.0,
-      child: Swiper(
-        layout: SwiperLayout.STACK,
-        itemWidth: 200.0,
-        itemBuilder: (BuildContext context,int index){
-          return new Image.network("http://via.placeholder.com/350x150",
-          fit: BoxFit.fill,);
-        },
-        itemCount: 3,
-        //pagination: new SwiperPagination(),
-        //control: new SwiperControl(),
-      ),
+    return CardSwiper(
+      peliculas: [1,2,3,4,5],
     );
   }
 }
